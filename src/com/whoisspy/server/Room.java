f21919d7c4fbd4e550c8c1b02dae4ef3cb91ada2
dsp85103 <dsp85103@gmail.com>
2019-12-22 17:29:19 +0800

    update Room and RoomsManager

--- src/com/whoisspy/server/Room.java
+++ src/com/whoisspy/server/Room.java
@@ -3,33 +3,64 @@ package com.whoisspy.server;
 import com.whoisspy.User;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Room {
 
-    private List<UserConnection> clients = new ArrayList<>();
-    private User roomOwner;
-    private int roomNumber;
+    private Map<String, UserConnection> clients = new HashMap<>();
+    private UserConnection roomOwner;
+    private int roomId;
     private String roomName;
     private int roomClientAmount;
-    private int roomClientCount;
     private String roomDescription;
     private boolean roomPrivate;
 
-    public Room(User roomOwner, String roomName, int roomClientAmount, String roomDescription, boolean roomPrivate) {
+    public Room(UserConnection roomOwner, Integer id, String roomName, int roomClientAmount, String roomDescription, boolean roomPrivate) {
         this.roomOwner = roomOwner;
+        this.roomId = id;
         this.roomName = roomName;
         this.roomClientAmount = roomClientAmount;
         this.roomDescription = roomDescription;
         this.roomPrivate = roomPrivate;
+
+        clients.put(roomOwner.getUser().getAccount(), roomOwner);
     }
 
     public boolean addPlayer(UserConnection userConnection) {
-        return clients.add(userConnection);
+        if (!clients.containsKey(userConnection.getUser().getAccount())) {
+            clients.put(userConnection.getUser().getAccount(), userConnection);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean removePlayer(UserConnection userConnection) {
-        return clients.remove(userConnection);
+        if (clients.containsKey(userConnection.getUser().getAccount())) {
+            clients.remove(userConnection.getUser().getAccount());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean changeOwner(UserConnection newOwner) {
+        this.roomOwner = newOwner;
+        return true;
+    }
+
+    public boolean isOwner(UserConnection userConn) {
+        return roomOwner == userConn;
+    }
+
+    public int getRoomClientAmount() {
+        return roomClientAmount;
+    }
+
+    public int getRoomClientCount() {
+        return clients.size();
     }
 
     public void start() {
--- src/com/whoisspy/server/RoomsManager.java
+++ src/com/whoisspy/server/RoomsManager.java
@@ -1,5 +1,81 @@
 package com.whoisspy.server;
 
+import java.util.*;
+
+// 房間需要的操作行為 統一由 room manager 處理
+
 public class RoomsManager {
+
+    private Map<Integer, Room> rooms = new HashMap<>();
+    private List<Integer> roomIds = new ArrayList<>();
+
+    public RoomsManager() {
+
+    }
+
+    public Room createRoom(UserConnection owner,
+                           String roomName,
+                           int roomClientAmount,
+                           String roomDescription,
+                           boolean roomPrivate) {
+
+        int roomId;
+
+        do {
+
+            roomId = (int) (Math.random() * 1000) +1;
+
+        } while (roomIds.contains(roomId));
+
+        Room createdRoom = new Room(owner, roomId, roomName, roomClientAmount, roomDescription, roomPrivate);
+        rooms.put(roomId, createdRoom);
+        return createdRoom;
+    }
+
+    public boolean joinRoom(UserConnection roomPlayer, Integer destRoomId) {
+        if (rooms.containsKey(destRoomId)) {
+            rooms.get(destRoomId).addPlayer(roomPlayer);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean leaveRoom(UserConnection roomPlayer, Integer roomId) {
+        if (rooms.containsKey(roomId)) {
+            Room room = rooms.get(roomId);
+            room.removePlayer(roomPlayer);
+            if (room.getRoomClientCount() == 0) {
+                return deleteRoom(roomId);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public boolean deleteRoom(Integer roomId) {
+        if (rooms.containsKey(roomId)) {
+            rooms.remove(roomId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean changeRoomOwner(UserConnection owner, Integer roomId, UserConnection newOwner) {
+        if (rooms.containsKey(roomId)) {
+            Room room = rooms.get(roomId);
+            if (room.isOwner(owner)) {
+                return room.changeOwner(newOwner);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
 
 }
